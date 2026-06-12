17534d9d1492817aa6cf0a8bc97b0711a9921a24
Maisha219 <maishamehrin219@gmail.com>
2026-06-12 10:52:14 +0600

    Revert "fix: set vite port to 5173"
    This reverts commit dbb7e99a8d86a7f457076c2141541a08898b562e.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={<Login />}
-        />
-
-        <Route
-          path="/register"
-          element={<Register />}
-        />
-
-        <Route
-          path="/dashboard"
-          element={<Dashboard />}
-        />
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
